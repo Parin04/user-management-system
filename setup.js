9b6f9a6a842be0368756f0e637ecb470341d748d
Parin04 <parinchuachan@gmail.com>
2025-06-25 00:55:22 +0700

    Initial commit

--- setup.js
+++ setup.js
@@ -4,11 +4,11 @@ require('dotenv').config();
 console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
 // Database connection
 const pool = new Pool({
-    user: process.env.DB_USER || 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_NAME || 'user_management',
-    password: process.env.DB_PASSWORD,   // เอา || 'password' ออก
-    port: process.env.DB_PORT || 5432,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
 });
 
 
