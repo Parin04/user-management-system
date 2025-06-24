fef0c6e854b26436149873fb1fea3590ff2170ae
Parin04 <parinchuachan@gmail.com>
2025-06-25 03:04:14 +0700

    Add debug routes and test interface

--- init-db.js
+++ init-db.js
@@ -2,12 +2,10 @@ const { Pool } = require('pg');
 const bcrypt = require('bcryptjs');
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
-  ssl: false
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 async function initializeDatabase() {
